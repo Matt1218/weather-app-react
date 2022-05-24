b9be089f951fdc170f84f55184d6a576d10f0d24
Matt1218 <welfleym@gmail.com>
2022-05-23 22:01:06 -0400

    api is fully functioning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 const api = {
-  key: 'api key',
+  key: 'api key here',
   base: 'https://api.openweathermap.org/data/2.5/',
 }
 
@@ -14,11 +14,12 @@ function App() {
       fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
       .then(res => res.json())
       .then(result => {
+        setWeather(result);
         setQuery('');
-        setWeather(result);});
         console.log(result);
-    }
+    });
   }
+}
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -41,19 +42,23 @@ function App() {
           onKeyPress={search}
           />
         </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
         <div className="location-box">
-          <div className="location">New York City, US</div>
+          <div className="location">{weather.name}, {weather.sys.country}</div>
           <div className="date">{dateBuilder(new Date())}</div>
         </div>
         <div className="weather-box">
           <div className="temp">
-            60°F
+            {Math.round(weather.main.temp)}°F
           </div>
-          <div className="weather">Cloudy</div>
+          <div className="weather">{weather.weather[0].main}</div>
         </div>
+        </div>
+        ) : ('')}
       </main>
     </div>
   )
 }
 
-export default App
+export default App;
