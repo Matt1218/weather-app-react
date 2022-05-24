c196b5eb3202eaa042eb31e2d091d5b4cf2c7d7e
Matt1218 <welfleym@gmail.com>
2022-05-23 20:41:42 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ const api = {
   key: "api key",
   base: "https://api.openweathermap.org/data/2.5/"
 }
+
 function App() {
-  return (
-    <div className="App">
-      
+  return ( 
+    <div className="app">
+    <main>
+    <div className="search-box">
+    <input type = "text"
+    className = "seach-bar"
+    placeholder = "Search..."
+    />
+    </div> 
+    </main> 
     </div>
   );
 }
